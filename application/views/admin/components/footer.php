97c36a0668eb65451214422251fdd055df4d3559
Ahsan Khan <ahsandev.creative>
2016-09-05 18:00:20 +0500

    admin panel

--- application/views/admin/components/footer.php
+++ application/views/admin/components/footer.php
@@ -74,7 +74,7 @@
 
         $("#yesApprove").on("click",function () {
             var hiddenModalUserID = $(this).parents(".modal-content").find("#hiddenUserID").val();
-            console.log(hiddenModalUserID);
+            //console.log(hiddenModalUserID);
             var postData = {id:hiddenModalUserID,value:"approve"};
             $.ajax({
                url:"<?=base_url();?>Admin/assessment_list",
@@ -85,13 +85,51 @@
                 type:"POST",
                 success:function (output) {
                     var data = output.split("::");
+                    if(data[0]=='OK'){
+                                         oTable ="";
+                        //Initialize Select2 Elements
+                        var regTableSelector = $("#regList");
+                        var url_DT = "<?=base_url();?>Admin/assessments_list/listing";
+                        var aoColumns_DT = [
+                            /* User ID */ {
+                                "mData": "UserID",
+                                "bVisible": true,
+                                "bSortable": true,
+                                "bSearchable": true
+                            },
+                            /* Full Name */ {
+                                "mData" : "FullName"
+                            },
+                            /* Email */ {
+                                "mData" : "Email"
+                            },
+                            /* Company */ {
+                                "mData" : "Company"
+                            },
+                            /*  Buisness */ {
+                                "mData" : "Business"
+                            },
+                            /* Last User Login */ {
+                                "mData": "Status"
+                            },
+                            /* Action Buttons */ {
+                                "mData" : "ViewEditActionButtons"
+                            }
+                        ];
+                        var HiddenColumnID_DT = "UserID";
+                        var sDom_DT = '<"H"r>t<"F"<"row"<"col-lg-6 col-xs-12" i> <"col-lg-6 col-xs-12" p>>>';
+                        commonDataTables(regTableSelector,url_DT,aoColumns_DT,sDom_DT,HiddenColumnID_DT);
+                        $('.approval-modal').modal().hide();
+                        $('body').removeClass('modal-open');
+                        $('.modal-backdrop').remove();
+                    }
                 }
             });
         });
 
         $("#noPending").on("click",function () {
             var hiddenModalUserID = $(this).parents(".modal-content").find("#hiddenUserID").val();
-            console.log(hiddenModalUserID);
+            //console.log(hiddenModalUserID);
             var postData = {id:hiddenModalUserID,value:"pending"};
             $.ajax({
                url:"<?=base_url();?>Admin/assessment_list",
@@ -102,6 +140,44 @@
                 type:"POST",
                 success:function (output) {
                     var data = output.split("::");
+                     if(data[0]=='OK'){
+                                         oTable ="";
+                        //Initialize Select2 Elements
+                        var regTableSelector = $("#regList");
+                        var url_DT = "<?=base_url();?>Admin/assessments_list/listing";
+                        var aoColumns_DT = [
+                            /* User ID */ {
+                                "mData": "UserID",
+                                "bVisible": true,
+                                "bSortable": true,
+                                "bSearchable": true
+                            },
+                            /* Full Name */ {
+                                "mData" : "FullName"
+                            },
+                            /* Email */ {
+                                "mData" : "Email"
+                            },
+                            /* Company */ {
+                                "mData" : "Company"
+                            },
+                            /*  Buisness */ {
+                                "mData" : "Business"
+                            },
+                            /* Last User Login */ {
+                                "mData": "Status"
+                            },
+                            /* Action Buttons */ {
+                                "mData" : "ViewEditActionButtons"
+                            }
+                        ];
+                        var HiddenColumnID_DT = "UserID";
+                        var sDom_DT = '<"H"r>t<"F"<"row"<"col-lg-6 col-xs-12" i> <"col-lg-6 col-xs-12" p>>>';
+                        commonDataTables(regTableSelector,url_DT,aoColumns_DT,sDom_DT,HiddenColumnID_DT);
+                        $('.approval-modal').modal().hide();
+                        $('body').removeClass('modal-open');
+                        $('.modal-backdrop').remove();
+                    }
                 }
             });
         });
